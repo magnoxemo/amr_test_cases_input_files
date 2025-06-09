3410a387e63a5c06850da37b5e5fe139066263c4
magnoxemo <walid.ahammed01@gmail.com>
2025-06-09 16:30:20 -0500

    debugging the script

--- amalgamation/mesh_amalgamation/clustering_strategies/create_eeid_and_aux_variable_input_file.py
+++ amalgamation/mesh_amalgamation/clustering_strategies/create_eeid_and_aux_variable_input_file.py
@@ -5,29 +5,30 @@ def argument_parser():
     ap = ArgumentParser()
     ap.add_argument('-f', dest="file_name")
     ap.add_argument('-id', dest="extra_element_integers",
-                    type=list,
                     help="names of the extra_element_integers")
+    ap.add_argument("-mesh_name", dest="mesh_file_name")
 
     return ap.parse_args()
 
 
 def add_eeid_block(arguments):
-    names_str = " ".join(str(name) for name in arguments.extra_element_integers)
-    value_str = " ".join(str(-1) for _ in arguments.extra_element_integers)
+    names_str = arguments.extra_element_integers.split(" ")
+    value_str = "-1 " * len(names_str)
+    names_formatted = " ".join(names_str)
 
     with open(arguments.file_name, "w") as f:
         f.write("[Mesh]\n")
         f.write("  [add_eeid_block]\n")
-        f.write("     type = ParsedElementIDMeshGenerator\n")
-        f.write(f"    extra_element_integer_names = '{names_str}\n'")
-        f.write(f"    values = '{value_str}'\n")
+        f.write("    type = ParsedElementIDMeshGenerator\n")
+        f.write(f"    extra_element_integer_names = '{names_formatted}'\n")
+        f.write(f"    values = '{value_str.strip()}'\n")
+        f.write(f"    input = {args.mesh_file_name}\n")
         f.write("  []\n")
         f.write("[]\n\n")
 
 
 def add_eeid_aux_variable_block(arguments):
-    names_str = " ".join(str(name) for name in arguments.extra_element_integers)
-
+    names_str = arguments.extra_element_integers.split(" ")
     with open(arguments.file_name, "a") as f:
         f.write("[AuxVariables]\n")
         for name in names_str:
@@ -43,7 +44,7 @@ def add_eeid_aux_variable_block(arguments):
 def add_eeid_copier_aux_kernel_block(arguments):
     with open(arguments.file_name, "a") as f:
         f.write("[AuxKernels]\n")
-        for name in arguments.extra_element_integers:
+        for name in arguments.extra_element_integers.split(" "):
             if name == " ":
                 continue
             f.write(f"  [store_{name}]\n")
@@ -59,4 +60,5 @@ if __name__ == "__main__":
     add_eeid_block(args)
     add_eeid_aux_variable_block(args)
     add_eeid_copier_aux_kernel_block(args)
+
 

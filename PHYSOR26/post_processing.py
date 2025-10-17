d5c5e0ffda39d17376dee6027d6d957920dae5af
Ebny Walid Ahammed <69362074+magnoxemo@users.noreply.github.com>
2025-10-16 22:42:24 -0500

    Fix typos in comments

--- PHYSOR26/post_processing.py
+++ PHYSOR26/post_processing.py
@@ -33,7 +33,7 @@ class post_processing_with_two_brain_cell:
         self.union_mesh_script = "common_mesh.i"
         
     def set_executable(self, executable_path):
-        """sets the absulute path of the executable"""
+        """sets the absolute path of the executable"""
         self.executable = executable_path
 
     def generate_union_mesh(self, time_step):
@@ -70,7 +70,7 @@ class post_processing_with_two_brain_cell:
 
         """
         now that the union mesh is generated I need to set it.
-        I can iterate through the files in that dir and pick up the most last time step
+        I can iterate through the files in that dir and pick up the latest time step
         """
         prefix = self.union_mesh_script[:-2] + "_out.e-s0"
         union_mesh_time_steps = [int(f[len(prefix):]) for f in os.listdir('.') if f.startswith(prefix) and f[len(prefix):].isdigit()]
@@ -94,7 +94,7 @@ class post_processing_with_two_brain_cell:
                 #need to delete that existing file otherwise my UO will just append data to it
                 os.system(f"rm -f {ref_dir_name}_{case}_flux_{tally}.csv ")
                 csv_file_names.append(f"UserObjects/{case}_{tally}_csv/csv_file_name={ref_dir_name}_{case}_flux_{tally}.csv")
-                #may I need to get rid of that flux later. But for now we are only using flux values
+                #I may need to get rid of that flux variable later. But for now we are only using flux values
 
         command = self.executable + " -i error_calculator.i --n-threads=28 " +" ".join(csv_file_names) + self.ref_test_mesh_arguments + self.union_mesh + f"variable={self.variable} variable_rel_error={variable}_rel_error"
 
